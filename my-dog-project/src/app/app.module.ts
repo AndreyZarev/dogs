ee1a100dafa365256322528d0ba9d08a8e44e951
tocode <andrey.zarev@outlook.com>
2024-02-26 15:55:14 +0200

    Update app.module.ts

--- my-dog-project/src/app/app.module.ts
+++ my-dog-project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { PlaygroundComponent } from './playground/playground.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    UserListComponent,
+    PlaygroundComponent
   ],
   imports: [
     BrowserModule
