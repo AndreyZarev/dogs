d3e5cb7fa4663f040e8f1e3e60e5fd61d2c01bc1
tocode <andrey.zarev@outlook.com>
2024-02-29 14:55:34 +0200

    Update input-field.component.ts

--- my-dog-project/src/app/input-field/input-field.component.ts
+++ my-dog-project/src/app/input-field/input-field.component.ts
@@ -1,21 +1,18 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
   styleUrls: ['./input-field.component.css']
 })
-export class InputFieldComponent {
+export class InputFieldComponent implements OnInit {
   handleSubmit(username: string) {
-    console.log(username);
-    //dont
-    //know
-    //how
-    //to
-    //clear
-    //values
-  }
-  //  ngOnInit() {
 
-  //  }
+    console.log(username);
+
+  }
+  ngOnInit() {
+    console.log("init");
+
+  }
 }
