b858361a7ff9ff6ecb2dd383c2d3768b6e282eb9
tocode <andrey.zarev@outlook.com>
2024-03-01 17:26:04 +0200

    Update app.module.ts

--- my-dog-project/src/app/app.module.ts
+++ my-dog-project/src/app/app.module.ts
@@ -2,19 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { PlaygroundComponent } from './playground/playground.component';
-import { InputFieldComponent } from './input-field/input-field.component';
-import { TestingFileComponent } from './testing-file/testing-file.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
-    UserListComponent,
-    PlaygroundComponent,
-    InputFieldComponent,
-    TestingFileComponent
+
   ],
   imports: [
     BrowserModule
